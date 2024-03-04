f34227831e15ef079e1d71325c6505f1b78000ef
Kurt Seifried <kurt@seifried.org>
2024-03-04 11:20:01 -0700

    line breaks

--- 2-process-cwe-summaries.sh
+++ 2-process-cwe-summaries.sh
@@ -6,6 +6,8 @@ rm -f 699-summary.csv
 rm -f 1000-summary.csv
 rm -f 1194-summary.csv
 
+dos2unix *
+
 ../generate-csv-data.py 699.xml 699-summary.csv
 ../generate-csv-data.py 1000.xml 1000-summary.csv
 ../generate-csv-data.py 1194.xml 1194-summary.csv
